9df2263615d653d1baca69c9fb58e3c9ed82752c
Marco Reni <reni.marco@gmail.com>
2021-12-09 10:50:20 +0100

    fix: path fix

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ import path from 'path';
 export const DOCS_FOLDER = 'docs';
 export const INDEX_FILE = 'index.html';
 
-export const tempPath = mkdtempSync(tmpdir());
+export const tempPath = mkdtempSync(path.join(tmpdir(), 'docs-publisher'));
 export const metadataFilePath = `${path.join(tempPath, 'metadata.json')}`;
 
 export type MetadataFile = {
