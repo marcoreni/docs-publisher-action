107ae97beee4501836569083c9c8a714a9123573
Marco Reni <reni.marco@gmail.com>
2024-10-21 20:04:56 +0200

    fix: build

--- src/strategies/package.ts
+++ src/strategies/package.ts
@@ -1,6 +1,5 @@
 import { readFile } from 'fs/promises';
 import { MetadataFile } from '../constants';
-import { execOutput, readMetadataFile } from '../utils';
 
 export async function packageStrategy(metadataFile: MetadataFile): Promise<string> {
   const packageJson = JSON.parse(await readFile('./package.json', 'utf-8')) as {
@@ -8,4 +7,6 @@ export async function packageStrategy(metadataFile: MetadataFile): Promise<strin
     version: string;
     location: string;
   };
+
+  return packageJson.version;
 }
